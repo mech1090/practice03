bca38dc0cef623a2f590c3422e73981411f750c0
Shahzad <mech1090@gmail.com>
2022-06-01 08:31:07 -0600

    validator  added

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,5 @@
-
+const {userValidateSchema} = require('../validator/user.validator')
+const User = require('../model/user')
 
 const getLoginForm = (req,res)=>{
     return res.render('login/layout')
@@ -7,8 +8,17 @@ const login = (req,res)=>{}
 const getSignupForm = (req,res)=>{
     return res.render('signup/layout')
 }
-const signup = (req,res)=>{
+const signup = async(req,res)=>{
     const {email,password} = req.body
+    const fields = {email,password}
+    const {error,value} = userValidateSchema(fields)
+    if(error){
+        res.render('signup/layout',{message:error.details[0].message})
+    }
+
+    const createUser = await User.create({email,password})
+    return res.render('signup/layout')
+
 }
 
 module.exports ={getLoginForm,login,getSignupForm,signup}
